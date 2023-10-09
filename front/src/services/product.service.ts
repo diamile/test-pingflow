720623d52e783865cdc3fb3e1f43781d9952d9b3
diamile <diamilesarr2006@gmail.com>
2023-10-09 18:35:09 +0200

    creation d'un product service et mise en place de la socket

---
+++ front/src/services/product.service.ts
@@ -0,0 +1,33 @@
+import io from 'socket.io-client';
+const socket = io('http://localhost:3000'); // Remplacez par l'URL de votre serveur Node.js
+
+export async function fetchAllProductFromService(){
+    try{
+       let response:any = [];
+        socket.on('connect', () => {
+            console.log('Connecté au serveur WebSocket');
+           
+        });
+        
+        socket.on('disconnect', () => {
+            console.log('Déconnecté du serveur WebSocket');
+        });
+
+        socket.emit('ok', 'sendata')
+        
+        console.log('response from',response)
+    } catch(err){
+        throw err;
+    }
+}
+
+export function getSocket(){
+    try{
+       
+     return socket
+        
+       
+    } catch(err){
+        throw err;
+    }
+}
